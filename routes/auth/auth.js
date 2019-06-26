fd76a0192959faea637963751c10bc9600501d91
Sam <Jeffcoat>
2019-06-25 19:30:11 -0500

    Register and post functions written

---
+++ routes/auth/auth.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const router = express.Router();
+
+const Users = require('../../data/models/users');
+const secrets = require('../../helpers/config/keys');
+
+router.post('/register', (req, res) => {
+    let user = req.body;
+    const hash = bcrypt.hashSync(user.password, 10);
+    user.password= hash;
+
+    Users.add(user)
+    .then(saved => {
+        res.status(201).json(saved)
+    })
+    .catch(error => {
+        res.status(500).json(error)
+    })
+})
+
+router.post('/login', (req, res) => {
+    let {username, password}= req.body;
+    Users.findBy({username})
+    .first()
+    .then(user => {
+        if(user && bcrypt.compareSync(password, user.password)){
+            const token = generateToken(user);
+            res.status(200).json({user, token});
+        } else {
+            res.status(401).json({
+                message: 'Invalid credentials'
+            })
+        }
+    })
+    .catch(error => 
+        res.status(500).json(error));
+})
+
+
+
+module.exports = router;

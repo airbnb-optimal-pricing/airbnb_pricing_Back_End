43dd5050dd3b6ebc6a2b6576d8fd1c2cff16cec9
Sam <Jeffcoat>
2019-06-27 13:11:54 -0500

    fixes seed error

--- data/seeds/001-users.js
+++ data/seeds/001-users.js
@@ -3,7 +3,7 @@ bcrypt = require('bcryptjs');
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('users').truncate()
+  return knex('users').del()
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([

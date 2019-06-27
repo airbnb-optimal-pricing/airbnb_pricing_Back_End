349da3e283813631385386e6290e3c0c909b579d
Sam <Jeffcoat>
2019-06-27 13:16:06 -0500

    seeds fixed

--- data/seeds/003-simple.js
+++ data/seeds/003-simple.js
@@ -6,8 +6,8 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('simple').insert([
         {id: 1, "zipcode": "90257", "bedrooms": 5, "bathrooms": 3},
-        { id: 1, "zipcode": "90267", "bedrooms": 1, "bathrooms": 1 },
-        { id: 1, "zipcode": "90258", "bedrooms": 4, "bathrooms": 1 }
+        { id: 2, "zipcode": "90267", "bedrooms": 1, "bathrooms": 1 },
+        { id: 3, "zipcode": "90258", "bedrooms": 4, "bathrooms": 1 }
       ]);
     });
 };

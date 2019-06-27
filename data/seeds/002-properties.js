a263def494d785a7cbc964d49ca8262205b28935
Sam <Jeffcoat>
2019-06-26 19:26:31 -0500

    Modifies properties router and seeds properties

---
+++ data/seeds/002-properties.js
@@ -0,0 +1,14 @@
+bcrypt = require('bcryptjs');
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('properties').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('properties').insert([
+        {id:1,  zipcode: "91210", property_type: "Apartment", room_type: "Entire home/apt", accommodates: 5, bathrooms: 5.0, bedrooms: 5.0, beds: 5.0, bed_type: "Real Bed" },
+        { id: 2, zipcode: "91304", property_type: "Apartment", room_type: "Entire home/apt", accommodates: 5, bathrooms: 4.0, bedrooms: 4.0, beds: 4.0, bed_type: "Real Bed" },
+        { id: 3, zipcode: "91304", property_type: "Apartment", room_type: "Entire home/apt", accommodates: 5, bathrooms: 4.0, bedrooms: 4.0, beds: 4.0, bed_type: "Real Bed" }
+      ]);
+    });
+};

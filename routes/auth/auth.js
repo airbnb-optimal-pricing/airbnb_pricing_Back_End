cf065ea52d70d039fc4a71e36bd9d34adcea4905
Sam <Jeffcoat>
2019-06-27 22:52:35 -0500

    changing restricted again

--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const router = express.Router();
-//const Properties = require('../../data/models/properties');
+const Properties = require('../../data/models/properties');
 const Users = require('../../data/models/users');
 const secrets = require('../../helpers/config/keys');
 
@@ -20,7 +20,24 @@ router.post('/register', (req, res) => {
         res.status(500).json(error)
     })
 })
+router.post('/userproperties', restricted, (req, res) => {
+    axios
+        .post("http://flask-env.kmg6svp6sr.us-east-2.elasticbeanstalk.com/prediction", {
+            headers: { accept: "application/json" },
+            zipcode: req.body.zipcode.toString(),
+            bedrooms: parseFloat(req.body.bedrooms),
+            bathrooms: parseFloat(req.body.bathrooms),
+            property_type: req.body.property_type.toString(),
+            room_type: req.body.room_type.toString(),
+            accommodates: (req.body.accommodates),
+            beds: (req.body.beds),
+            bed_type: req.body.bed_type.toString()
+        })
+        .then(response =>
+            res.status(200).json(response.data))
 
+        .catch(err => res.status(500).json(err.response));
+})
 router.post('/login', (req, res) => {
     let {username, password}= req.body;
     Users.findBy({username})
